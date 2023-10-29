912793881c8e5f7ddccd5b9324ee0b8d342d2a3e
Dawid Klos <klosdawid12@gmail.com>
2023-10-29 15:53:36 +0000

    style: add hero background image

--- src/components/homepage/hero/Hero.tsx
+++ src/components/homepage/hero/Hero.tsx
@@ -7,21 +7,26 @@ export const Hero: React.FC = () => {
   return (
     <section className={styles.section}>
       <div className={styles.container}>
-        <h1 className={styles.title}>Niezawodne rozwiązania elektryczne</h1>
+        <h1 className={styles.title}>
+          Poznaj niezawodne rozwiązania <span>elektryczne</span>
+        </h1>
         <p className={styles.subtitle}>
-          Jesteśmy zaufanym partnerem w zakresie wszystkich potrzeb elektrycznych - za każdym razem dostarczamy
+          Jesteśmy zaufanym partnerem w zakresie wszystkich potrzeb elektrycznych - za każdym razem dostarczając
           doskonałość.
         </p>
         <Link className={styles.link} href="/kontakt">
-          Skontaktuj się teraz
+          Skontaktuj się
         </Link>
       </div>
       <div className={styles.aside}>
-        <Image src={timeIcon} alt="" priority />
+        <Image className={styles.icon} src={timeIcon} alt="" priority />
         <p className={styles.asideTitle}>Krótki okres oczekiwania</p>
         <p className={styles.asideSubtitle}>
           Nasza wysoka dostępność gwrantuje, że nie będziesz musiał czekać na kluczowe usługi elektryczne.
         </p>
+      </div>
+      <div className={styles.background}>
+        <p className={styles.backgroundTitle}>JK</p>
       </div>
     </section>
   );
