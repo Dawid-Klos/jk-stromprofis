9495bbf6a71b99dba603822006bd6a6bfd704c7b
Dawid Klos <klosdawid12@gmail.com>
2023-11-06 18:24:11 +0000

    refactor: file formatting

--- src/components/Header/Mobile/Mobile.tsx
+++ src/components/Header/Mobile/Mobile.tsx
@@ -3,14 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {
-    Navbar,
-    NavbarContent,
-    NavbarMenuToggle,
-    NavbarMenu,
-    NavbarMenuItem,
-    NavbarBrand
-} from "@nextui-org/react";
+import { Navbar, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarBrand } from "@nextui-org/react";
 
 import styles from "./Mobile.module.scss";
 
@@ -20,72 +13,52 @@ import Accordion from "./Accordion";
 import type { menuItemsTypes } from "@config/menuItems";
 
 type Props = {
-    menuItems: menuItemsTypes;
+  menuItems: menuItemsTypes;
 };
 
-
 export const Mobile = ({ menuItems }: Props) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toogle = () => {
-        setIsOpen(!isOpen);
-    };
+  const toogle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <Navbar
-            classNames={
-                {
-                    base: styles.nav,
-                    wrapper: styles.wrapper,
-                    item: styles.item,
-                }
-            }
-            height="75px"
-            isMenuOpen={isOpen}
-            onMenuOpenChange={setIsOpen}
+  return (
+    <Navbar
+      classNames={{
+        base: styles.nav,
+        wrapper: styles.wrapper,
+        item: styles.item,
+      }}
+      height="75px"
+      isMenuOpen={isOpen}
+      onMenuOpenChange={setIsOpen}
+    >
+      <NavbarContent>
+        <NavbarBrand>
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            <Image src={logo} alt="JK-Stromprofis" width={64} height={54} />
+          </Link>
+        </NavbarBrand>
 
-        >
-            <NavbarContent>
-                <NavbarBrand>
-                    <Link href="/" onClick={() => setIsOpen(false)}>
-                        <Image
-                            src={logo}
-                            alt="JK-Stromprofis"
-                            width={64}
-                            height={54}
-                        />
-                    </Link>
-                </NavbarBrand>
+        <NavbarMenuToggle aria-label={isOpen ? "Close menu" : "Open menu"} className={styles.toggle} />
+      </NavbarContent>
 
-                <NavbarMenuToggle
-                    aria-label={isOpen ? "Close menu" : "Open menu"}
-                    className={styles.toggle}
-                />
-            </NavbarContent>
-
-            <NavbarMenu className={styles.menu}>
-
-                {menuItems.map(({ title, href, list }) => {
-                    return (
-                        list ? (
-                            <NavbarMenuItem key={title}>
-                                <Accordion
-                                    title={title}
-                                    list={list}
-                                    setIsOpen={setIsOpen}
-                                />
-                            </NavbarMenuItem>
-                        ) : (
-                            <NavbarMenuItem key={title}>
-                                <Link className={styles.link} href={href} onClick={toogle}>
-                                    {title}
-                                </Link >
-                            </NavbarMenuItem>
-                        )
-                    )
-                })}
-            </NavbarMenu >
-        </Navbar >
-    )
-
+      <NavbarMenu className={styles.menu}>
+        {menuItems.map(({ title, href, list }) => {
+          return list ? (
+            <NavbarMenuItem key={title}>
+              <Accordion title={title} list={list} setIsOpen={setIsOpen} />
+            </NavbarMenuItem>
+          ) : (
+            <NavbarMenuItem key={title}>
+              <Link className={styles.link} href={href} onClick={toogle}>
+                {title}
+              </Link>
+            </NavbarMenuItem>
+          );
+        })}
+      </NavbarMenu>
+    </Navbar>
+  );
 };
