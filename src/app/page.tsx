3c881ac87c6ba696e382f45e9d06e79ec9e938c9
Tomasz Klos <tomasz.klos@nerds.family>
2023-11-06 19:04:29 +0100

    feat: create CtaSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Hero from "@components/Homepage/Hero";
 import Trust from "@components/Homepage/Trust";
 import Testimonials from "@components/Homepage/Testimonials";
+import CtaSection from "@components/common/CtaSection";
+
+import questionImage from "@assets/images/question-image.png";
 
 export default function Home() {
   return (
@@ -8,6 +11,17 @@ export default function Home() {
       <Hero />
       <Trust />
       <Testimonials />
+      <CtaSection
+        subtitle="Komunikacja"
+        title="Wciąż masz pytania?"
+        content="Dzięki silnej reputacji i świetnym rekomendacjom, JK-Stromprofis jest bez wątpienia wiodącym wyborem w zakresie profesjonalnych usług elektrycznych. Nie musisz wierzyć nam na słowo - przekonaj się o kompetencjach i energii naszego zespołu na własnej skórze."
+        image={{
+          src: questionImage,
+          alt: "Wciąż masz pytania?",
+          width: 260,
+          height: 260,
+        }}
+      />
     </>
   );
 }
---
+++ src/components/common/CtaSection/CtaSection.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import Image, { StaticImageData } from "next/image";
+
+import styles from "./CtaSection.module.scss";
+
+import Hgroup from "@components/common/Hgroup";
+
+type Props = {
+  title: string;
+  subtitle: string;
+  content: string;
+  image: {
+    src: StaticImageData;
+    alt: string;
+    width: number;
+    height: number;
+  };
+};
+
+export const CtaSection = ({ title, subtitle, content, image }: Props) => {
+  const { src, alt, width, height } = image;
+
+  return (
+    <section className={styles.section}>
+      <Hgroup title={title} subtitle={subtitle} divider={false} />
+
+      <p className={styles.content}>{content}</p>
+
+      <Image
+        className={styles.image}
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+      />
+
+      <Button className={styles.link} href="/kontakt" as={Link}>
+        Skontaktuj się teraz
+      </Button>
+    </section>
+  );
+};
