2c62178f0e5194df7377b779376b83c193fa0509
Dawid Klos <klosdawid12@gmail.com>
2023-11-08 16:25:00 +0000

    feat: add image under Services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 import Hero from "@components/Homepage/Hero";
 import Trust from "@components/Homepage/Trust";
 import Testimonials from "@components/Homepage/Testimonials";
 import Services from "@components/Homepage/Services";
+
+import styles from "./page.module.scss";
+import cityImag from "@assets/images/home-city-desktop-xl.png";
 
 export default function Home() {
   return (
@@ -9,6 +14,18 @@ export default function Home() {
       <Hero />
       <Trust />
       <Services />
+      <div className={styles.background}>
+        <Image
+          className={styles.image}
+          src={cityImag}
+          alt="a view of city during a night"
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+        />
+      </div>
+
       <Testimonials />
     </>
   );
