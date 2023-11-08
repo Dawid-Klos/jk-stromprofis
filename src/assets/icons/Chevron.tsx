93f41f756461f0478742c474531fdbf0aac471a4
Dawid Klos <klosdawid12@gmail.com>
2023-11-08 16:15:38 +0000

    feat: allow for className props in Chevron svg

--- src/assets/icons/Chevron.tsx
+++ src/assets/icons/Chevron.tsx
@@ -1,6 +1,17 @@
-const Chevron = () => {
+type Props = {
+  className?: string;
+};
+
+const Chevron = ({ className }: Props) => {
   return (
-    <svg fill="none" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      className={className}
+      fill="none"
+      height="16"
+      viewBox="0 0 24 24"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
         stroke="#d3c3bf"
