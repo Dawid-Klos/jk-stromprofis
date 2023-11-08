5b87acaebae0a094c0ba4e4f498931f550dc5326
Tomasz Klos <tomasz.klos@nerds.family>
2023-11-08 13:17:39 +0100

    feat: display cta section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,21 @@
+import CtaSection from "@components/common/CtaSection";
+
+import questionImage from "@assets/images/question-image.png";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <CtaSection
+        subtitle="Komunikacja"
+        title="Wciąż masz pytania?"
+        content="Dzięki silnej reputacji i świetnym rekomendacjom, JK-Stromprofis jest bez wątpienia wiodącym wyborem w zakresie profesjonalnych usług elektrycznych. Nie musisz wierzyć nam na słowo - przekonaj się o kompetencjach i energii naszego zespołu na własnej skórze."
+        image={{
+          src: questionImage,
+          alt: "Wciąż masz pytania?",
+          width: 260,
+          height: 260,
+        }}
+      />
+    </>
+  );
 }
