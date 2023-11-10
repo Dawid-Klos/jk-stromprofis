af22f75e8e86e4ec7b2195a508830503d2bc70ab
Tomasz Klos <tomasz.klos@nerds.family>
2023-11-10 19:01:17 +0100

    fix: remove dead imports

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,6 @@
 import Mobile from "./Mobile";
 import Desktop from "./Desktop";
 
-import useBreakpoint from "@hooks/useBreakpoint";
-import { Breakpoint } from "@config/enums";
 import { menuItems } from "@config/menuItems";
 
 export const Header = () => {
