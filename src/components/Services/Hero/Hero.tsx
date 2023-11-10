35596340cfa7886f371ff145ec9614be1124ba18
Tomasz Klos <tomasz.klos@nerds.family>
2023-11-10 21:09:54 +0100

    feat: create Hero component

---
+++ src/components/Services/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import styles from "./Hero.module.scss";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export const Hero = ({ title, subtitle }: Props) => {
+  return (
+    <section className={styles.section}>
+      <hgroup className={styles.hgroup}>
+        <h1 dangerouslySetInnerHTML={{ __html: title }}></h1>
+        <p>{subtitle}</p>
+      </hgroup>
+    </section>
+  );
+};
