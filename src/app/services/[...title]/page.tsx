c7f9b0cd956a6abd1c44d01b742eb97f46647a69
Tomasz Klos <tomasz.klos@nerds.family>
2023-11-10 19:08:08 +0100

    feat: display cta section

--- src/app/services/[...title]/page.tsx
+++ src/app/services/[...title]/page.tsx
@@ -1,15 +1,32 @@
+import CtaSection from "@components/common/CtaSection";
 
 import { services } from "@config/services";
 import { convertTitleToHref } from "@utils/functions";
 
+import ctaImage from "@assets/images/services-cta.png";
+
 export default function Page({ params }: { params: { title: string } }) {
-    const title = params.title.toString();
+  const title = params.title.toString();
 
-    const service = services.find((service) => convertTitleToHref(service.title) === title);
+  const service = services.find(
+    (service) => convertTitleToHref(service.title) === title
+  );
 
-    return (
-        <>
-        <h1>{title}</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>{title}</h1>
+
+      <CtaSection
+        subtitle="kiedy tego potrzebujesz"
+        title="Twój zaufany partner"
+        content="Przenieś swój dom lub firmę na wyższy poziom dzięki naszym najwyższej jakości instalacjom elektrycznym. Niezależnie od tego, czy chcesz zmodernizować swój obecny system, czy rozpocząć zupełnie nowy projekt, pozwól JK-Stromprofis być Twoim zaufanym partnerem."
+        image={{
+          src: ctaImage,
+          alt: "",
+          width: 260,
+          height: 260,
+        }}
+      />
+    </>
+  );
 }
