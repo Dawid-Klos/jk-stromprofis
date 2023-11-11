0e218940c3934fd6583dfff598667c92d37f0b90
Dawid Klos <klosdawid12@gmail.com>
2023-11-11 14:34:19 +0000

    feat: create validatiob schema rules

--- src/config/formValidation.ts
+++ src/config/formValidation.ts
@@ -4,12 +4,16 @@ const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 export const schema = object({
-  name: string().required(),
-  email: string().email().required(),
-  phone: string().required().matches(phoneRegExp, "Phone number is not valid").min(8, "too short").max(9, "too long"),
-  client: string().required(),
-  message: string().required(),
-  policy: bool().required(),
+  name: string().required("Name ist erforderlich").min(5, "Der Name ist zu kurz").max(25, "Der Name ist zu lang"),
+  email: string().email("E-Mail Adresse ist falsch").required("Eine E-Mail Adresse ist erforderlich"),
+  phone: string()
+    .required("Eine Telefonnummer ist erforderlich")
+    .matches(phoneRegExp, "Die Telefonnummer ist falsch und/oder enthält eine Vorwahl")
+    .min(9, "Rufnummer zu kurz eingegeben")
+    .max(11, "Die eingegebene Rufnummer ist zu kurz"),
+  client: string().required("Der Kundentyp ist erforderlich"),
+  message: string().required("Kurze Nachricht erforderlich").min(30, "Die Nachricht ist zu kurz"),
+  policy: bool().oneOf([true], "Sie müssen die Datenschutzbestimmungen akzeptieren."),
 });
 
 export const defaultValues = {
