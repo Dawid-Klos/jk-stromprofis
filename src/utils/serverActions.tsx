ef5bb4696be3a2302aa506b1116eea4c1caffb80
Dawid Klos <klosdawid12@gmail.com>
2023-11-11 18:15:28 +0000

    feat: send email to gmail account

--- src/utils/serverActions.tsx
+++ src/utils/serverActions.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import nodemailer from "nodemailer";
 import { FormData, schema } from "@config/formValidation";
 
 export const handleContactFormSubmit = async (data: FormData) => {
@@ -8,10 +9,11 @@ export const handleContactFormSubmit = async (data: FormData) => {
   try {
     const validatedData = await schema.validate(data, { abortEarly: false });
 
+    const email = await sendEmail(validatedData);
+
     return {
       statusCode: 200,
       headers: {
-        Location: "/",
         message: "success",
       },
     };
@@ -19,9 +21,48 @@ export const handleContactFormSubmit = async (data: FormData) => {
     return {
       statusCode: 400,
       headers: {
-        Location: "/kontakt",
         message: "One or more issue has been found: " + errors,
       },
     };
   }
+};
+
+const sendEmail = async (data: FormData) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: { user: process.env.EMAIL, pass: process.env.PASSWORD },
+  });
+
+  const body = createHTMLBody(data);
+
+  let mailOptions = {
+    from: data.email,
+    to: process.env.EMAIL,
+    subject: `Wiadomość od ${data.name}`,
+    html: body,
+  };
+
+  let info = await transporter.sendMail(mailOptions);
+
+  return info;
+};
+
+const createHTMLBody = (data: FormData) => {
+  const { name, businessName, email, client, phone, message } = data;
+
+  return `
+    <h1>Wiadomość od ${name}</h1>
+
+    <p><b>Typ klienta:</b>  ${client === "customer" ? "Prywatny" : "Firma"}</p>
+    ${businessName ? `<p><b>Nazwa firmy:</b>  ${businessName}</p>` : ""}
+    <p><b>Adres e-mail:</b>  <a href="mailto:${email}">${email}</a></p>
+    <p><b>Numer telefonu:</b>  <a href="tel:${phone}">${phone}</a></p>
+    <br />
+    <hr />
+    <br />
+    <h2>Wiadomość:</h2>
+    <p>${message}</p>
+  `;
 };
