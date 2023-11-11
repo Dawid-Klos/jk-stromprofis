37d1b849b3045dbb028412e0f44ede901dca0b31
Dawid Klos <klosdawid12@gmail.com>
2023-11-11 12:52:43 +0000

    feat: add useFormLogic hook in Form component

--- src/components/Contact/Form/Form.tsx
+++ src/components/Contact/Form/Form.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Controller } from "react-hook-form";
+
 import Button from "@components/common/Button";
+import { useFormLogic } from "@hooks/useFormLogic";
 
 import Input from "./Input";
 import Select from "./Select";
@@ -10,19 +13,55 @@ import Checkbox from "./Checkbox";
 import styles from "./Form.module.scss";
 
 export const Form = () => {
+  const { control, handleSubmit, onSubmit, errors } = useFormLogic();
+
   return (
-    <form className={styles.form}>
-      <Input type="name" label="Imie i nazwisko" autoComplete="name" />
-      <Input type="email" label="Email" autoComplete="email" />
-      <Input type="tel" label="Numer telefonu" autoComplete="tel-local" />
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="name"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <Input type="name" label="Imie i nazwisko" autoComplete="name" value={value} onChange={onChange} />
+        )}
+      />
+      <Controller
+        name="email"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <Input type="email" label="Email" autoComplete="email" value={value} onChange={onChange} />
+        )}
+      />
+      <Controller
+        name="phone"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <Input type="tel" label="Numer telefonu" autoComplete="tel-local" value={value} onChange={onChange} />
+        )}
+      />
 
-      <Select />
+      <Controller
+        name="client"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => <Select value={value} onChange={onChange} />}
+      />
 
-      <Textarea />
-      <div className={styles.checkboxWrapper}>
-        <Checkbox />
-        <p className={styles.paragraph}>Akceptuje politykę prywatności</p>
-      </div>
+      <Controller
+        name="message"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => <Textarea value={value} onChange={onChange} />}
+      />
+
+      <Controller
+        name="policy"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => <Checkbox value={value} onChange={onChange} />}
+      />
 
       <Button className={styles.button} element="button" variant="secondary">
         Wyślij wiadomość
