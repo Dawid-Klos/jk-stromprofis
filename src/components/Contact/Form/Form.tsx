f6a95128a8822449dc282920af674171a1e7b8d7
Dawid Klos <klosdawid12@gmail.com>
2023-11-11 20:31:18 +0000

    feat: handle form submission result display

--- src/components/Contact/Form/Form.tsx
+++ src/components/Contact/Form/Form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { Controller, set } from "react-hook-form";
+import { Controller } from "react-hook-form";
 
 import Button from "@components/common/Button";
 import { useFormLogic } from "@hooks/useFormLogic";
@@ -11,11 +11,15 @@ import Input from "./Input";
 import Select from "./Select";
 import Textarea from "./Textarea";
 import Checkbox from "./Checkbox";
+import { SubmitResult } from "./SubmitResult";
 
 import styles from "./Form.module.scss";
 
 export const Form = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
+
   const { control, watch, errors, handleSubmit } = useFormLogic();
 
   const isCompany = watch("client") === "business";
@@ -27,131 +31,145 @@ export const Form = () => {
       const res = await handleContactFormSubmit(data);
 
       if (res.statusCode === 200 && res.headers.message === "success") {
-        console.log("Code: ", res.statusCode);
-        console.log("Message: ", res.headers.message);
+        setIsFormSubmitted(true);
+        setIsSuccess(true);
       } else {
-        console.log("Code: ", res.statusCode);
-        console.log("Message: ", res.headers.message);
+        setIsFormSubmitted(true);
+        setIsSuccess(false);
       }
     } catch (error) {
-      console.log(error);
+      setIsFormSubmitted(false);
+      setIsSuccess(false);
     }
 
     setIsLoading(false);
   };
 
+  if (isFormSubmitted && !isLoading) {
+    return <SubmitResult isSuccess={isSuccess} setIsFormSubmitted={setIsFormSubmitted} setIsSuccess={setIsSuccess} />;
+  }
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
-      <Controller
-        name="name"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            type="text"
-            label="Imie i nazwisko"
-            autoComplete="name"
-            isInvalid={!!errors.name}
-            errorMessage={errors.name?.message}
-            value={value}
-            onChange={onChange}
-          />
-        )}
-      />
+    <article className={styles.article}>
+      <h2 className={styles.title}>Wpisz dane kontaktowe</h2>
 
-      <Controller
-        name="email"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            type="email"
-            label="Email"
-            autoComplete="email"
-            isInvalid={!!errors.email}
-            errorMessage={errors.email?.message}
-            value={value}
-            onChange={onChange}
-          />
-        )}
-      />
-
-      <Controller
-        name="phone"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            type="tel"
-            label="Numer telefonu"
-            autoComplete="tel-local"
-            isInvalid={!!errors.phone}
-            errorMessage={errors.phone?.message}
-            value={value}
-            onChange={onChange}
-          />
-        )}
-      />
-
-      <Controller
-        name="client"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Select isInvalid={!!errors.client} errorMessage={errors.client?.message} value={value} onChange={onChange} />
-        )}
-      />
-
-      {isCompany && (
+      <form className={`${styles.form} ${isLoading && styles.opacity}`} onSubmit={handleSubmit(onSubmit)} noValidate>
         <Controller
-          name="businessName"
+          name="name"
           control={control}
           rules={{ required: true }}
           render={({ field: { onChange, value } }) => (
             <Input
               type="text"
-              label="Nazwa firmy"
-              autoComplete="organization"
-              isInvalid={!!errors.businessName}
-              errorMessage={errors.businessName?.message}
+              label="Imie i nazwisko"
+              autoComplete="name"
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message}
               value={value}
               onChange={onChange}
             />
           )}
         />
-      )}
 
-      <Controller
-        name="message"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Textarea
-            isInvalid={!!errors.message}
-            errorMessage={errors.message?.message}
-            value={value}
-            onChange={onChange}
+        <Controller
+          name="email"
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              type="email"
+              label="Email"
+              autoComplete="email"
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message}
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
+
+        <Controller
+          name="phone"
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              type="tel"
+              label="Numer telefonu"
+              autoComplete="tel-local"
+              isInvalid={!!errors.phone}
+              errorMessage={errors.phone?.message}
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
+
+        <Controller
+          name="client"
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Select
+              isInvalid={!!errors.client}
+              errorMessage={errors.client?.message}
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
+
+        {isCompany && (
+          <Controller
+            name="businessName"
+            control={control}
+            rules={{ required: true }}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type="text"
+                label="Nazwa firmy"
+                autoComplete="organization"
+                isInvalid={!!errors.businessName}
+                errorMessage={errors.businessName?.message}
+                value={value}
+                onChange={onChange}
+              />
+            )}
           />
         )}
-      />
 
-      <Controller
-        name="policy"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Checkbox
-            isInvalid={!!errors.policy}
-            errorMessage={errors.policy?.message}
-            value={value}
-            onChange={onChange}
-          />
-        )}
-      />
+        <Controller
+          name="message"
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Textarea
+              isInvalid={!!errors.message}
+              errorMessage={errors.message?.message}
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
 
-      <Button className={styles.button} element="button" variant="secondary" isLoading={isLoading}>
-        Wyślij wiadomość
-      </Button>
-    </form>
+        <Controller
+          name="policy"
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Checkbox
+              isInvalid={!!errors.policy}
+              errorMessage={errors.policy?.message}
+              value={value}
+              onChange={onChange}
+            />
+          )}
+        />
+
+        <Button className={styles.button} element="button" variant="secondary" isLoading={isLoading}>
+          Wyślij wiadomość
+        </Button>
+      </form>
+    </article>
   );
 };
